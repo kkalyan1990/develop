5599b2923f3c6f430f75f7631b8ae0657a378b72
Kalyan <katragaddakalyan2@gmail.com>
2018-02-21 20:47:23 -0500

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Test01Component } from './test01/test01.component';
+import { Test02Component } from './test02/test02.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Test01Component,
+    Test02Component
   ],
   imports: [
     BrowserModule
